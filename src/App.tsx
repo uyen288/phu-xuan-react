445b6e52022d473b4cc9041ebfdbec218549a500
uyen88 <hohuonguyen06@gmail.com>
2026-08-21 08:05:11 +0700

    feat: khởi tạo phu-xuan-react với Vite + React + TypeScript + ESLint + Prettier

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>phu-xuan-react</h1>
+      <p>Dự án xuyên suốt học phần Web FrontEnd nâng cao</p>
+      <p>Trường Đại học Phú Xuân - Khoa CNTT</p>
+      <p>Sinh viên: Hồ Hương Uyên</p>
+    </div>
+  )
+}
+
+export default App
