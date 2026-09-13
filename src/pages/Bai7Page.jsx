307c314d8ab9bc6e9b4b185153306f7fe8493e1c
uyen288 <hohuonguyen06@gmail.com>
2026-09-13 10:21:51 +0700

    Lab 2: chọn nhiều địa danh, truyền tham số, data-*

--- src/pages/Bai7Page.jsx
+++ src/pages/Bai7Page.jsx
@@ -1,11 +1,13 @@
 import '../styles/Bai7.css'
 import ThuSuKien from '../components/Bai7/ThuSuKien'
+import DanhSachDiaDanh from '../components/Bai7/DanhSachDiaDanh'
 
 export default function Bài7Page() {
   return (
     <main className="trang-Bài7">
       <h1>Bài 7 — Quản lý sự kiện trong React</h1>
       <ThuSuKien />
+      <DanhSachDiaDanh />
     </main>
   )
 }
